e46911a011b8daeca1efd5a32e4672ccf0634038
Oshea Brown <oshea.brown@gmail.com>
2016-09-04 11:57:13 -0600

    Attatched Scripts to Prefabs
    I attached the scripts for the player and blocks so now you should be
    able to control the "Ship" with "A" and "D" as well as be able to
    destroy a block if you hit it with the ball.

--- TheDyingPlanet/Assets/Scripts/BlockScript.cs
+++ TheDyingPlanet/Assets/Scripts/BlockScript.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class BrickScrpit : MonoBehaviour
+public class BlockScript : MonoBehaviour
 {
 
     void OnCollisionEnter2D (Collision2D coll)
