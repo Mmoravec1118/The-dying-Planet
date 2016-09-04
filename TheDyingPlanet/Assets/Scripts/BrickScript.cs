ed0d9c63b593abfce05295dbe7eb2402e127cf81
Oshea Brown <oshea.brown@gmail.com>
2016-09-04 11:42:26 -0600

    Added Prefabs and Scripts
    I added scripts for player movement and for the blocks to disappear when
    they are hit by an object tagged "Ball". The prefabs I added are just
    place holders until we have our art assets.

---
+++ TheDyingPlanet/Assets/Scripts/BrickScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BrickScrpit : MonoBehaviour
+{
+
+    void OnCollisionEnter2D (Collision2D coll)
+    {
+        // if a ball collides with a block, destroy the block
+        if (coll.gameObject.tag == "Ball")
+        {
+            Destroy(gameObject);
+        }
+    }
+	
+}
---
+++ TheDyingPlanet/Assets/Scripts/SpaceshipMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpaceshipMovement : MonoBehaviour
+{
+    public float shipSpeed;
+    float horizontalVelocity;
+    Vector2 tempVelocity;
+    	
+	// Update is called once per frame
+	void Update ()
+    {
+        // store the ships horizontal input axis
+        horizontalVelocity = Input.GetAxisRaw("Horizontal");
+
+        // calculate the velocity of the ship based on the horizontal input 
+        tempVelocity = new Vector2(horizontalVelocity * shipSpeed, 0);
+        tempVelocity.Normalize();
+        tempVelocity *= shipSpeed;
+
+        // apply the velocity to the ships rigid body
+        GetComponent<Rigidbody2D>().velocity = tempVelocity;	
+	}
+}
