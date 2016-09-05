e6cc8b80b8489744c7324b3b3d7e0b774019e365
Oshea Brown <oshea.brown@gmail.com>
2016-09-05 12:58:51 -0600

    Simple Character Selection
    I added an extremely simple main menu for now, but it does have the
    capability to select a different ship based on which button is pressed.

---
+++ TheDyingPlanet/Assets/Scripts/CharacterSelectButton.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class CharacterSelectButton : MonoBehaviour {
+
+    public void FlatShipSelection ()
+    {
+        PlayerPrefs.SetInt("CharacterChoice", 0);
+        SceneManager.LoadScene("Scene1");
+    }
+
+    public void CurvedShipSelection()
+    {
+        PlayerPrefs.SetInt("CharacterChoice", 1);
+        SceneManager.LoadScene("Scene1");
+    }
+}
---
+++ TheDyingPlanet/Assets/Scripts/CharacterSpawn.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterSpawn : MonoBehaviour
+{
+
+    public GameObject[] playerShips = new GameObject[2];
+
+    // Use this for initialization
+    void Start ()
+    {
+        if (PlayerPrefs.GetInt("CharacterChoice") == 0)
+        {
+            GameObject.Instantiate(playerShips[0], transform.position, transform.rotation);
+        }
+        else if (PlayerPrefs.GetInt("CharacterChoice") == 1)
+        {
+            Vector3 rotation = new Vector3(0, 0, 90);
+            GameObject.Instantiate(playerShips[1], transform.position, Quaternion.Euler(rotation));
+        }
+	}
+}
